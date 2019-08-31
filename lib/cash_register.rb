9c90f581437d5a9a6b99ac1d285850d5d141d78c
yaoyu bao <yaobao5354@gmail.com>
2019-08-31 00:28:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :price
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
+    @last_item_price = []
+  end 
+  
+  def add_item(item, price, quantity = 1)
+    @total = @total + price * quantity 
+    @last_item_price << price 
+    while quantity > 0
+      @items << item
+      quantity -= 1 
+    end 
+  end 
+  
+  def apply_discount
+    if discount == 0 
+      "There is no discount to apply."
+    else 
+      @total = @total - @total * discount/100
+      "After the discount, the total comes to $#{@total}."
+    end 
+  end 
+  
+  def void_last_transaction
+    
+    
+  end 
+  
+end 
